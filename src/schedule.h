47c981527b0a4129b4a82529d0875c409f7c6942
gettogetto <643274003@qq.com>
2016-11-24 18:29:49 +0800

    first commit

---
+++ src/schedule.h
@@ -0,0 +1,38 @@
+#ifndef SCHEDULE_H
+#define SCHEDULE_H
+
+#include<ucontext.h>
+#include<unordered_map>
+#include<memory>
+#include<functional>
+class Coroutine;
+class Schedule
+{
+public:
+    using CoMap = std::unordered_map<int,std::shared_ptr<Coroutine>>;
+    enum{_STACKSIZE = 1024*1024,_MAX_CO_NUM = 10};
+   // enum{DEAD,READY,RUNNING,SUSPEND};
+    Schedule();
+    ~Schedule();
+
+    void add(std::shared_ptr<Coroutine> coroutine);
+    void remove(int coroutine_id);
+    void resume(int coroutine_id);
+    void yield();
+
+    int getStatus(int coroutine_id);
+    int getCurCoroutineId(){return _cur_run_id;}
+private:
+    //void _save_stack(void *top);
+    char _stack[_STACKSIZE];
+    ucontext_t _nextContext;
+    int _co_cur_num;
+    int _co_max_num;
+    int _cur_run_id;
+    CoMap _map;
+
+
+    friend  void mainfunc(uint32_t low32,uint32_t high32);
+};
+
+#endif // SCHEDULE_H
---
+++ test/main.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include"schedule.h"
+#include"coroutine.h"
+#include<memory>
+using namespace std;
+struct args{
+    int n;
+};
+
+void func(std::shared_ptr<Schedule> s,void *ud){
+    struct args* arg = (args*)ud;
+    int start = arg->n;
+    for(int i=0;i<50;i++){
+        std::cout<<"coroutine "<<s->getCurCoroutineId()<<" "<<start+i<<std::endl;
+        s->yield();
+    }
+}
+void test(){
+    args arg1{1};
+    args arg2{101};
+    std::shared_ptr<Schedule> s(new Schedule());
+    std::shared_ptr<Coroutine> co1(new Coroutine(s,std::bind(&func,s,&arg1),0));
+    std::shared_ptr<Coroutine> co2(new Coroutine(s,std::bind(&func,s,&arg2),1));
+
+    s->add(co1);
+    s->add(co2);
+    int count = 0;
+    while(co1->getStatus()||co2->getStatus()){
+        if(count==5) s->remove(0);
+        if(count==40) s->remove(1);
+        s->resume(0);
+        s->resume(1);
+        count++;
+    }
+}
+
+int main(int argc, char *argv[])
+{
+    test();
+    return 0;
+
+}
