7f71404fdb9477394b613576d641b92fa1a306ca
gettogetto <643274003@qq.com>
2016-11-24 19:47:14 +0800

    namespace JC

--- src/schedule.h
+++ src/schedule.h
@@ -5,34 +5,35 @@
 #include<unordered_map>
 #include<memory>
 #include<functional>
-class Coroutine;
-class Schedule
-{
-public:
-    using CoMap = std::unordered_map<int,std::shared_ptr<Coroutine>>;
-    enum{_STACKSIZE = 1024*1024,_MAX_CO_NUM = 10};
-   // enum{DEAD,READY,RUNNING,SUSPEND};
-    Schedule(int num=10);
-    ~Schedule();
+namespace JC{
+	class Coroutine;
+	class Schedule
+	{
+	public:
+	    using CoMap = std::unordered_map<int,std::shared_ptr<Coroutine>>;
+	    enum{_STACKSIZE = 1024*1024,_MAX_CO_NUM = 10};
+	   // enum{DEAD,READY,RUNNING,SUSPEND};
+	    Schedule(int num=10);
+	    ~Schedule();
 
-    void add(std::shared_ptr<Coroutine> coroutine);
-    void remove(int coroutine_id);
-    void resume(int coroutine_id);
-    void yield();
+	    void add(std::shared_ptr<Coroutine> coroutine);
+	    void remove(int coroutine_id);
+	    void resume(int coroutine_id);
+	    void yield();
 
-    int getStatus(int coroutine_id);
-    int getCurCoroutineId(){return _cur_run_id;}
-private:
-    //void _save_stack(void *top);
-    char _stack[_STACKSIZE];
-    ucontext_t _nextContext;
-    int _co_cur_num;
-    int _co_max_num;
-    int _cur_run_id;
-    CoMap _map;
+	    int getStatus(int coroutine_id);
+	    int getCurCoroutineId(){return _cur_run_id;}
+	private:
+	    //void _save_stack(void *top);
+	    char _stack[_STACKSIZE];
+	    ucontext_t _nextContext;
+	    int _co_cur_num;
+	    int _co_max_num;
+	    int _cur_run_id;
+	    CoMap _map;
 
 
-    friend  void mainfunc(uint32_t low32,uint32_t high32);
-};
-
+	    friend  void mainfunc(uint32_t low32,uint32_t high32);
+	};
+}
 #endif // SCHEDULE_H
